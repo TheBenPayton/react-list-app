e7d4cdb52045ce76ba660d30f09b2cf533d4ffea
Payton Long <33674328+TheBenPayton@users.noreply.github.com>
2018-12-11 19:18:38 -0600

    Updated css and Material UI navbar files.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,14 +3,20 @@ import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Icon from '@material-ui/core/Icon';
+
+function link(){
+    console.log('Clicked');
+}
 const NavBar = () => {
     return(
         <div className="navi">
-            <AppBar position="fixed" color="secondary">
+            <AppBar style={{backgroundColor: 'indianred'}} position="fixed" color="secondary">
                 <Toolbar>
                     <Typography variant="title" color="inherit">
-                        Listy,the greatest list building buddy.
+                        Listy, the greatest list building buddy
                     </Typography>
+                    <Icon id='icons' onClick={ link() }>mood</Icon>
                 </Toolbar>
             </AppBar>
         </div>
